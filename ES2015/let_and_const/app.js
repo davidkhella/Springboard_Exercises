92443e7c4c64e86bd6c224f6d306181d3a605c0f
David Khella <david.khella.87@gmail.com>
2023-07-30 17:05:12 -0700

    let and constexercise

---
+++ ES2015/let_and_const/app.js
@@ -0,0 +1,9 @@
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+/* Quiz */
+1. You can reassign and redeclare with var, but you can not redeclare using the let keyword.
+2. You can reassign and redeclare with var, but you cannot reassign or redeclare using the const keyword.
+3. You can reassign with let, but you cannot reassign using the const keyword.
+4. Variable are hoisted to the top scope they are declared in. It's value is undefined when hoisted.
