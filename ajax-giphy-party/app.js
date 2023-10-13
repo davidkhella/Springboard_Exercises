3586e3cfcc47b59d1f10fea7398a705605e1f4e2
David <david.khella.87@gmail.com>
2023-10-13 16:03:39 -0700

    completed logic and functionality

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -3,6 +3,42 @@
 // - Allow the user to search for as many GIFs as they would like and keep appending them to the page
 // - Allow the user to remove all of the GIFs by clicking a button
 
+const $gifArea = $('#gif-area');
+const $searchInput = $('#search-field');
+
+$('form').on('submit', async function(e) {
+    e.preventDefault();
+    
+    let searchTerm = $searchInput.val();
+    $searchInput.val('');
+
+    const response = await axios.get('http://api.giphy.com/v1/gifs/search', {
+        params: {
+        q: searchTerm,
+        api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+        }
+    });
+    
+    console.log(response.data);
+    addGif(response.data)
+})
+
+function addGif(res) {
+    let resDataLength = res.data.length;
+    if (resDataLength) {
+        let randomImgIndex = Math.floor(Math.random() * resDataLength);
+        let $newDiv = $('<div>');
+        let $newGifImg = $('<img>', {
+            src: res.data[randomImgIndex].images.original.url
+        });
+        $newDiv.append($newGifImg);
+        $gifArea.append($newDiv);
+    }
+}
+
+$('#delete-btn').on('click', function() {
+    $gifArea.empty();
+})
 
 
 
