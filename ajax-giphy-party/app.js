02b706c7344f2e7dc90697d22cc3e1a025397f68
David Khella <david.khella.87@gmail.com>
2023-10-15 15:43:29 -0700

    final touches to styling

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -3,43 +3,41 @@
 // - Allow the user to search for as many GIFs as they would like and keep appending them to the page
 // - Allow the user to remove all of the GIFs by clicking a button
 
-const $gifArea = $('#gif-area');
-const $searchInput = $('#search-field');
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search-field");
 
-$('form').on('submit', async function(e) {
-    e.preventDefault();
-    
-    let searchTerm = $searchInput.val();
-    $searchInput.val('');
+$("form").on("submit", async function (e) {
+  e.preventDefault();
 
-    const response = await axios.get('http://api.giphy.com/v1/gifs/search', {
-        params: {
-        q: searchTerm,
-        api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
-        }
-    });
-    
-    console.log(response.data);
-    addGif(response.data)
-})
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
+    },
+  });
+
+  console.log(response.data);
+  addGif(response.data);
+});
 
 function addGif(res) {
-    let resDataLength = res.data.length;
-    if (resDataLength) {
-        let randomImgIndex = Math.floor(Math.random() * resDataLength);
-        let $newDiv = $('<div>');
-        let $newGifImg = $('<img>', {
-            src: res.data[randomImgIndex].images.original.url
-        });
-        $newDiv.append($newGifImg);
-        $gifArea.append($newDiv);
-    }
+  let resDataLength = res.data.length;
+  if (resDataLength) {
+    let randomImgIndex = Math.floor(Math.random() * resDataLength);
+    let $newDiv = $("<div>").addClass("col-sm-12 col-lg-4");
+    let $newGifImg = $("<img>", {
+      src: res.data[randomImgIndex].images.original.url,
+    }).addClass("card-img m-1");
+    $newDiv.append($newGifImg);
+    $gifArea.append($newDiv);
+  }
 }
 
-$('#delete-btn').on('click', function() {
-    $gifArea.empty();
-})
-
-
+$("#delete-btn").on("click", function () {
+  $gifArea.empty();
+});
 
 console.log("Let's get this party started!");
