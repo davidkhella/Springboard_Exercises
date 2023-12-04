248fd61edd6f7444a8545f4afab3e1d25977f3ad
David Khella <david.khella.87@gmail.com>
2023-12-03 20:16:20 -0800

    notes 12032023

--- python-practice/python-practice.py
+++ python-practice/python-practice.py
@@ -18,12 +18,12 @@
 # end_fill()
 # done()
 
-import turtle
-colors = ['red','purple','blue', 'green','orange','yellow']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(360):
-    t.pencolor(colors[x % 6])
-    t.width(x/100 + 1)
-    t.forward(x)
-    t.left(59)
+# import turtle
+# colors = ['red','purple','blue', 'green','orange','yellow']
+# t = turtle.Pen()
+# turtle.bgcolor('black')
+# for x in range(360):
+#     t.pencolor(colors[x % 6])
+#     t.width(x/100 + 1)
+#     t.forward(x)
+#     t.left(59)
