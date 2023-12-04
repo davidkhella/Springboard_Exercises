4ebbcb0c3876f69145afd99dac4f3b5f327a36a4
David Khella <david.khella.87@gmail.com>
2023-12-03 20:15:19 -0800

    notes 12032023

---
+++ python-practice/python-practice.py
@@ -0,0 +1,29 @@
+# def add(a,b):
+#     """tests
+#     >>>sum(5,5)
+#     10
+    
+#     """
+    
+#     return a + b
+
+# from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
+
+import turtle
+colors = ['red','purple','blue', 'green','orange','yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x % 6])
+    t.width(x/100 + 1)
+    t.forward(x)
+    t.left(59)
