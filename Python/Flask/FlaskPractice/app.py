04c12d2de6268008a322182bd4f9686e495c9efe
David Khella <david.khella.87@gmail.com>
2024-01-01 21:13:00 -0800

    flask boggle

--- Python/Flask/FlaskPractice/app.py
+++ Python/Flask/FlaskPractice/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from unittest import TestCase
 
 app = Flask(__name__)
 
@@ -6,3 +7,15 @@ app = Flask(__name__)
 def say_hello():
     """Shows hello page"""
     return render_template("hello.html")
+
+def adder(x, y):
+    return x + y
+
+# assert adder(2, 5) == 7
+# assert adder(2, 7) == 10
+
+class AdditionTestCase(TestCase):
+    """Examples of unit tests."""
+
+    def test_adder(self):
+        self.assertEqual(adder(2, 2), 4)
