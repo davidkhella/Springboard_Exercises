923485a85399b4cf8b3b1dd6dff46b99d3cf4909
David Khella <david.khella.87@gmail.com>
2024-01-02 20:40:18 -0800

    01022024 flask boggle

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,3 +1,13 @@
+from flask import Flask, request, render_template, redirect, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "diamond123"
+
 boggle_game = Boggle()
+
+@app.route('/')
+def start_game():
+    title = "Boggle Game"
+    board = boggle_game.make_board()
+    return render_template('index.html', title=title, board=board)
